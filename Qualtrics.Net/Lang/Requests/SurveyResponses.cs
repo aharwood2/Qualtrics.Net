b81eac50851cebac0a46e708059af333693af6c5
Adrian Harwood <adrian.harwood@manchester.ac.uk>
2021-04-27 14:22:33 +0100

    Exposed request fields as public

--- Qualtrics.Net/Lang/Requests/SurveyResponses.cs
+++ Qualtrics.Net/Lang/Requests/SurveyResponses.cs
@@ -7,41 +7,41 @@ namespace Qualtrics.Net.Lang.Requests.SurveyResponses
 {
     // Create Response Request //
 
-    internal class CreateResponseRequest : Request
+    public class CreateResponseRequest : Request
     {
         [JsonProperty("values")]
-        internal Values Values { get; set; }
+        public Values Values { get; set; }
     }
 
     // This will need to be extended based on the survey structure //
     abstract public class Values
     {
         [JsonProperty("distributionChannel")]
-        internal string DistributionChannel { get; set; }
+        public string DistributionChannel { get; set; }
 
         [JsonProperty("duration")]
-        internal int Duration { get; set; }
+        public int Duration { get; set; }
 
         [JsonProperty("endDate")]
-        internal string EndDate { get; set; }
+        public string EndDate { get; set; }
 
         [JsonProperty("finished")]
-        internal int Finished { get; set; }
+        public int Finished { get; set; }
 
         [JsonProperty("locationLatitude")]
-        internal string Latitude { get; set; }
+        public string Latitude { get; set; }
 
         [JsonProperty("locationLongitude")]
-        internal string Longitude { get; set; }
+        public string Longitude { get; set; }
 
         [JsonProperty("progress")]
-        internal int Progress { get; set; } = 1;
+        public int Progress { get; set; } = 1;
 
         [JsonProperty("startDate")]
-        internal string StartDate { get; set; }
+        public string StartDate { get; set; }
 
         [JsonProperty("userLanguage")]
-        internal string UserLanguage { get; set; }
+        public string UserLanguage { get; set; }
     }
 
     // TODO: Add more JSON structures for remaining parts of Survey Responses API
